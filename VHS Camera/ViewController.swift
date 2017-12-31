82f35b086dec112069871d017607aa7df89ef19e
JaekyungLee-B2LiNK <jklee@b2link.co.kr>
2018-01-01 07:56:15 +0900

    Initial Commit

---
+++ VHS Camera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VHS Camera
+//
+//  Created by Jay on 01/01/2018.
+//  Copyright © 2018 Jay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
